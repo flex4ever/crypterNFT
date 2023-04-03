2a833d3a6dfdddc6a26dfcf7243aa88570746b29
LeonidCher <4ls2005@mail.ru>
2023-04-03 22:07:10 +0400

    1 секция с багами

---
+++ js/main_page-js/script.js
@@ -0,0 +1,16 @@
+
+    $('.main-slider-line').slick({
+        Infinite: true,
+        dots: false,
+        autoplay: false,
+        appendArrows: $('.descNft__arrows-group')
+    })
+ 
+    console.log('FGHGFH')
+// $('.main-slider-line').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+// });
