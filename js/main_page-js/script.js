bcd92197ff0f6998a868750bcc85afb74ae11298
LeonidCher <4ls2005@mail.ru>
2023-04-11 01:27:09 +0400

    tablet addaptive 1024px, full page

--- js/main_page-js/script.js
+++ js/main_page-js/script.js
@@ -3,16 +3,13 @@ const sellersBtn = document.querySelector('.pop-sellers__timeframe'),
     sellersWindow = document.querySelector('.pop-sellers__show-hide-block'),
     sellersContent = document.querySelectorAll('.pop-sellers__show-hide-text'),
     sellersContentBtn = document.getElementById('timeframeContent')
-
-
-
 sellersBtn.addEventListener('click', () => {
     sellersWindow.classList.toggle('pop-sellers__show-hide-block_active')
 
 })
 dropDownList(sellersContent, 'pop-sellers__show-hide-text_active',
- 'pop-sellers__show-hide-block_active', sellersContentBtn, sellersWindow)
-
+    'pop-sellers__show-hide-block_active', sellersContentBtn, sellersWindow)
+    ////////////////////////////////////////////////
 const recentlyBtn = document.getElementById('recetly-added-btn'),
     recentlyHideBlock = document.getElementById('recetly-added-hide-block'),
     recentlyTitle = document.getElementById('d-main__date'),
@@ -21,9 +18,57 @@ const recentlyBtn = document.getElementById('recetly-added-btn'),
 recentlyBtn.addEventListener('click', () => {
     recentlyHideBlock.classList.toggle('show-hide-block_active_height-recently')
 })
-dropDownList(recentlyText, 'show-hide-text_active','show-hide-block_active_height-recently',
- recentlyTitle, recentlyHideBlock )
-    
+dropDownList(recentlyText, 'show-hide-text_active', 'show-hide-block_active_height-recently',
+    recentlyTitle, recentlyHideBlock)
+    ////////////////////////////////////////
+const priceBtn = document.getElementById('d-main__priceBtn'),
+    priceHideBlock = document.getElementById('d-main__price-block'),
+    priceTitle = document.getElementById('d-main__priceBtnContent'),
+    priceText = document.querySelectorAll('.d-main__price-hide-text')
+toggleClass(priceBtn, priceHideBlock, 'show-hide-block_active_height-price')
+dropDownList(priceText, 'show-hide-text_active', 'show-hide-block_active_height-price', priceTitle, priceHideBlock)
+    //////////////////////////////////////////  
+const likesBtn = document.getElementById('d-main__LikesBtn'),
+    likesHideBlock = document.getElementById('d-main__likes-hide-block'),
+    likesTitle = document.getElementById('d-main__LikesContentBtn'),
+    likesText = document.querySelectorAll('.d-main__likes-hide-text')
+toggleClass(likesBtn, likesHideBlock, 'show-hide-block_active_height-price')
+dropDownList(likesText, 'show-hide-text_active', 'show-hide-block_active_height-price', likesTitle, likesHideBlock)
+    /////////////////////////////////////////////////
+
+const creatorBtn = document.getElementById('d-main__creatorBtn'),
+    creatorHideBlock = document.getElementById('d-main__creatorShowBlock'),
+    creatorTitle = document.getElementById('d-main__creatorContentBtn'),
+    creatorText = document.querySelectorAll('.d-main__creator-drop-list-btn-txt')
+
+toggleClass(creatorBtn, creatorHideBlock, 'show-hide-block_active_height-price')
+dropDownList(creatorText, 'show-hide-text_active', 'show-hide-block_active_height-price', creatorTitle, creatorHideBlock)
+    /////////////////////////////////////////////////
+
+const inputRange = document.querySelector('.d-main__input-block-input'),
+    inputWindow = document.querySelector('.d-main__input-block-input-num')
+inputRange.addEventListener('input', function(){
+    console.log(this.value)
+    inputWindow.style.left = this.value * 22 + 'px'
+    inputWindow.innerHTML = this.value
+
+})
+////////////////////////////////////////////////////
+const filtresBlock = document.querySelector('.d-main__filtres-block'),
+    filtresBtn = document.querySelector('.d-main__filter-btn'),
+    borderBlockBtn = document.querySelector('.d-main__main-buttons')
+    cardsBlockDiscover = document.querySelector('.dicover__cards-block')
+filtresBtn.addEventListener('click', ()=>{
+    filtresBlock.classList.toggle('d-main__filtres-block_active')
+    borderBlockBtn.classList.toggle('d-main__main-buttons_active')
+    cardsBlockDiscover.classList.toggle('dicover__cards-block_active-margin')
+    console.log('dfgfgd')
+})
+
+
+
+
+
 
 function dropDownList(array, classAdd, classRemove, elementOne, elementTwo) {
     array.forEach((el) => {
@@ -36,6 +81,11 @@ function dropDownList(array, classAdd, classRemove, elementOne, elementTwo) {
             elementTwo.classList.remove(classRemove)
 
         })
+    })
+}
+function toggleClass(btn, blockToggle, toggleClass) {
+    btn.addEventListener('click', () => {
+        blockToggle.classList.toggle(toggleClass)
     })
 }
 
--- js/main_page-js/slick-settings.js
+++ js/main_page-js/slick-settings.js
@@ -1,4 +1,4 @@
-const privet = 'fggfgf'
+
 $('.main-slider-line').slick({
     Infinite: true,
     appendArrows: $('.descNft__arrows-group'),
@@ -8,20 +8,46 @@ $('.main-slider-line').slick({
 $('.pop-sellers__slider-line').slick({
     slidesToShow: 5,
     infinite: false,
-    centerPadding: '60px',
-    adaptiveHeight: true,
     adaptiveHeight: true,
     slidesToScroll: 4,
+    responsive:[
+        {
+            breakpoint: 1025,
+            settings:{
+                appendArrows: $('.pop-sellers__buttons-addaptive'),
+                slidesToShow: 4,
+            }
+        }
+    ]
 });
 $('.hot-bid-slider').slick({
     slidesToShow: 4,
     infinite: false,
-    centerPadding: '60px',
     adaptiveHeight: true,
     adaptiveHeight: true,
     slidesToScroll: 4,
-    appendArrows: $('.hot-bid__slider-buttons-block')
+    appendArrows: $('.hot-bid__slider-buttons-block'),
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }
+    ]
 });
 $('.hot-collections__card-block').slick({
     slidesToShow: 3,
+    infinite: false,
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                appendArrows: $('.hot-collections__buttons-block-addaptive')
+            }
+        }
+    ]
 })
